00e209b3f25a64be025665fa2ec9eab3516275dd
Abrar Takib 🚩 <takibahamed26@gmail.com>
2026-07-05 00:52:03 +0600

    Add DIVISIONS data structure for regions in Bangladesh

---
+++ data.py
@@ -0,0 +1,22 @@
+DIVISIONS = {
+    "Dhaka": {
+        "districts": ["Dhaka", "Gazipur", "Narayanganj"],
+        "food": ["Kacchi Biryani", "Tehari", "Fuchka"],
+        "places": ["Lalbagh Fort", "Ahsan Manzil", "Hatirjheel"]
+    },
+    "Chattogram": {
+        "districts": ["Chattogram", "Cox's Bazar", "Rangamati"],
+        "food": ["Mezban Beef", "Sea Fish", "Kala Bhuna"],
+        "places": ["Cox's Beach", "Patenga", "Foy's Lake"]
+    },
+    "Sylhet": {
+        "districts": ["Sylhet", "Moulvibazar", "Habiganj"],
+        "food": ["Shatkora Beef", "Pitha"],
+        "places": ["Jaflong", "Ratargul", "Tea Garden"]
+    },
+    "Khulna": {
+        "districts": ["Khulna", "Bagerhat", "Satkhira"],
+        "food": ["Chui Jhal Meat", "Hilsa Fish"],
+        "places": ["Sundarbans", "Shait Gumbad Mosque"]
+    }
+}
